308d7bc0c2c1f1bad9db0196b94567b33d3f0d2d
iansu-twn <iansu.job@gmail.com>
2024-05-29 12:38:09 +0800

    feat: Ipost login

--- src/grabber/ipost.py
+++ src/grabber/ipost.py
@@ -1,6 +1,8 @@
 import argparse
+import time
 
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 
 
 class Ipost():
@@ -9,6 +11,48 @@ class Ipost():
         self.uid = uid
         self.pwd = pwd
         self.driver = webdriver.Chrome()
+
+    def login(self):
+        self.driver.get(
+            "https://ipost.post.gov.tw/pst/home.html"
+        )
+        # check scam msg
+        try:
+            elem = self.driver.find_element(
+                "xpath",
+                "//*[@id='modal']/div[2]/button"
+            )
+            elem.click()
+        except NoSuchElementException:
+            pass
+
+        # change login page
+        time.sleep(3)
+        self.driver.find_element(
+            "xpath",
+            "//*[@id='content_wh']/div[1]/div/ul/li[1]/a"
+        ).click()
+
+        # fill in the login info
+        id = self.driver.find_element(
+            "xpath",
+            "//*[@id='cifID']"
+        )
+        id.send_keys(self.id)
+
+        uid = self.driver.find_element(
+            "xpath",
+            "//*[@id='userID_1_Input']"
+        )
+        uid.send_keys(self.uid)
+
+        pwd = self.driver.find_element(
+            "xpath",
+            "//*[@id='userPWD_1_Input']"
+        )
+        pwd.send_keys(self.pwd)
+
+        # ocr verification code
 
 
 if __name__ == "__main__":
@@ -19,3 +63,4 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     client = Ipost(args.id, args.uid, args.pwd)
+    client.login()

daf9874623285625d84528cf5a80f724ed509aa3
iansu-twn <iansu.job@gmail.com>
2024-05-31 13:48:43 +0800

    Feat: login

--- src/grabber/cathy.py
+++ src/grabber/cathy.py
@@ -1,4 +1,5 @@
 import argparse
+import time
 
 from selenium import webdriver
 
@@ -10,6 +11,28 @@ class Cathy:
         self.pwd = pwd
         self.driver = webdriver.Chrome()
 
+    def login(self):
+        self.driver.get("https://www.cathaybk.com.tw/mybank/")
+
+        # fill in the login info
+        id = self.driver.find_element("xpath", "//*[@id='CustID']")
+        id.send_keys(self.id)
+
+        uid = self.driver.find_element("xpath", "//*[@id='UserIdKeyin']")
+        uid.send_keys(self.uid)
+
+        pwd = self.driver.find_element("xpath", "//*[@id='PasswordKeyin']")
+        pwd.send_keys(self.pwd)
+
+        btn_login = self.driver.find_element(
+            "xpath", "//*[@id='divCUBNormalLogin']/div[2]/button"
+        )
+        btn_login.click()
+        time.sleep(3)
+
+    def close_driver(self):
+        self.driver.close()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("CATHY INFO")
@@ -17,3 +40,6 @@ if __name__ == "__main__":
     parser.add_argument("--uid", type=str)
     parser.add_argument("--pwd", type=str)
     args = parser.parse_args()
+    client = Cathy(args.id, args.uid, args.pwd)
+    client.login()
+    client.close_driver()

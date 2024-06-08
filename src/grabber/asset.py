088e2667d9e7b222515154d7765bcd3010840500
iansu-twn <iansu.job@gmail.com>
2024-06-08 11:39:50 +0800

    Update: error handling

--- src/grabber/asset.py
+++ src/grabber/asset.py
@@ -2,15 +2,18 @@ import configparser
 import json
 import logging
 import re
+import time
 from datetime import datetime
 
 import requests
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 class Asset:
-    def __init__(self, args):
-        info = self.information(args)
+    def __init__(self, exchange, config_path="./config/info.ini"):
+        self.exchange = exchange
+        info = self.information(self.exchange, config_path)
         self.id = info.get("id")
         self.uid = info.get("uid")
         self.pwd = info.get("pwd")
@@ -20,30 +23,43 @@ class Asset:
         self.app_id = info.get("app_id")
         self.base_url = info.get("base_url")
         self.timestamp = self.getTimestamp()
-        self.driver = webdriver.Chrome()
 
-    def information(self, arg):
+        # configure chrome options
+        chrome_options = Options()
+        # chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
+        self.driver = webdriver.Chrome(options=chrome_options)
+
+    def information(self, arg, config_path):
         cfg = configparser.ConfigParser()
-        cfg.read("./config/info.ini")
-        info = {}
-        for option in cfg.options(arg):
-            info[option] = cfg.get(arg, option)
-        return info
+        try:
+            cfg.read(config_path)
+            if arg not in cfg.sections():
+                raise ValueError(f"Configuration section {arg} not found")
+            info = {}
+            for option in cfg.options(arg):
+                info[option] = cfg.get(arg, option)
+            return info
+
+        except Exception as e:
+            logging.error(f"Error reading configuration: {e}")
+            raise
 
     def extract_cash(self, elem):
         try:
             text = elem.text.strip()
             cash = re.sub(r"[^\d.-]", "", text)
-            return int(cash)
+            return float(cash)
         except ValueError:
             logging.info("Couldn't extract cash")
-            return 0
+            return 0.0
         except Exception as e:
-            print(e)
-            return 0
+            logging.info(f"Unexcepted error in extract_cash: {e}")
+            return 0.0
 
-    def close_driver(self):
+    def close_driver(self, arg):
         self.driver.close()
+        logging.info(f"{arg} LOGOUT SUCCESSFUL")
 
     def getTimestamp(self):
         return str(round(datetime.now().timestamp() * 1000))
@@ -54,13 +70,31 @@ class Asset:
     def getHeaders(self, signature):
         raise NotImplementedError
 
-    def send_requests(self, method, endpoint, body=None):
+    def send_requests(self, method, endpoint, body=None, retries=5, sleep=1):
         body_json = json.dumps(body) if body else ""
         signature = self.getSignature(method, endpoint, body_json)
         headers = self.getHeaders(signature)
         url = f"{self.base_url}{endpoint}"
-        res = requests.request(method, url, headers=headers, data=body_json)
 
-        if res.status_code == 200:
-            return res.json()
-        raise Exception(f"Error: {res.status_code}, {res.text}")
+        attempt = 0
+        while attempt < retries:
+            try:
+                res = requests.request(
+                    method, url, headers=headers, data=body_json
+                )  # noqa: E501
+                res.raise_for_status()
+                return res.json()
+            except requests.exceptions.HTTPError as http_err:
+                logging.error(
+                    f"HTTP error occurred: {res.status_code}:{http_err}"
+                )  # noqa: E501
+            except requests.exceptions.RequestException as req_err:
+                logging.error(f"Request error occurred: {req_err}")
+            except Exception as err:
+                logging.error(f"Other error occurred: {err}")
+
+            attempt += 1
+            time.sleep(sleep)
+        raise Exception(
+            f"Failed to get a successful response after {retries} attempts"
+        )  # noqa: E501
--- src/grabber/firstrade.py
+++ src/grabber/firstrade.py
@@ -1,102 +1,140 @@
 import argparse
+import logging
 import time
 
 import pandas as pd
 from asset import Asset
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class Firstrade(Asset):
-    def __init__(self, arg):
-        super().__init__(arg)
+    def __init__(self, exchange):
+        super().__init__(exchange)
 
     def login(self):
-        self.driver.get(
-            "https://invest.firstrade.com/cgi-bin/login?ft_locale=zh-tw"
-        )  # noqa: E501
-        # fill in the login info
-        uid = self.driver.find_element("xpath", "//*[@id='username']")
-        uid.send_keys(self.uid)
+        try:
+            self.driver.get(
+                "https://invest.firstrade.com/cgi-bin/login?ft_locale=zh-tw"
+            )  # noqa: E501
+            # fill in the login info
+            uid = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, "//*[@id='username']")
+                )  # noqa: E501
+            )
+            uid.send_keys(self.uid)
 
-        pwd = self.driver.find_element("xpath", "//*[@id='password']")
-        pwd.send_keys(self.pwd)
+            pwd = self.driver.find_element(By.XPATH, "//*[@id='password']")
+            pwd.send_keys(self.pwd)
 
-        btn_login = self.driver.find_element("xpath", "//*[@id='loginButton']")
-        btn_login.click()
+            btn_login = self.driver.find_element(
+                By.XPATH, "//*[@id='loginButton']"
+            )  # noqa: E501
+            btn_login.click()
 
-        btn_confirm = self.driver.find_element(
-            "xpath", "/html/body/div/main/div/div/div[3]/a"
-        )
-        btn_confirm.click()
+            btn_confirm = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, "/html/body/div/main/div/div/div[3]/a")
+                )
+            )
+            btn_confirm.click()
 
-        pin = self.driver.find_element("xpath", "//*[@id='pin']")
-        pin.send_keys(self.pin)
+            pin = self.driver.find_element(By.XPATH, "//*[@id='pin']")
+            pin.send_keys(self.pin)
 
-        btn_pin = self.driver.find_element(
-            "xpath", "//*[@id='form-pin']/div[2]/button"
-        )  # noqa: E501
-        btn_pin.click()
-        time.sleep(5)
+            btn_pin = self.driver.find_element(
+                By.XPATH, "//*[@id='form-pin']/div[2]/button"
+            )  # noqa: E501
+            btn_pin.click()
+            time.sleep(5)
+        except Exception as e:
+            logging.error(f"Error during login: {e}")
+            self.close_driver(self.exchange)
 
     def info(self):
-        self.driver.find_element(
-            "xpath", "//*[@id='myaccount_link']/a"
-        ).click()  # noqa: E501
-        time.sleep(3)
-
-        cash = float(
+        try:
+            logging.info(f"{self.exchange} LOGIN SUCCESSFUL")
             self.driver.find_element(
-                "xpath",
-                "//*[@id='maincontent']/div/table/tbody/tr/td[1]/"
-                + "div/div[2]/table[1]/tbody/tr[1]/td[1]",
-            )
-            .text.replace(",", "")
-            .strip()[1:]
-        )
+                By.XPATH, "//*[@id='myaccount_link']/a"
+            ).click()  # noqa: E501
+            time.sleep(5)
 
-        self.driver.find_element(
-            "xpath", "//*[@id='myaccount_menu']/li[2]/a/span"
-        ).click()
-        time.sleep(3)
-        html = self.driver.page_source
-        soup = BeautifulSoup(html, "html.parser")
-        elem = soup.find("table", {"id": "positiontable"})
-        stock = self.parse_table(elem)
-        return cash, stock
+            # cash
+            cash_text = self.driver.find_element(
+                By.XPATH,
+                "//*[@id='maincontent']/div/table/tbody/tr/td[1]/div/div[2]/table[1]/tbody/tr[1]/td[1]",  # noqa: E501
+            ).text
+            cash = float(cash_text.replace(",", "").strip()[1:])
+
+            # stock
+            self.driver.find_element(
+                By.XPATH, "//*[@id='myaccount_menu']/li[2]/a/span"
+            ).click()
+            time.sleep(3)
+            html = self.driver.page_source
+            soup = BeautifulSoup(html, "html.parser")
+            elem = soup.find("table", {"id": "positiontable"})
+            stock = self.parse_table(elem)
+            return cash, stock
+        except Exception as e:
+            logging.error(f"Error retrieving account info: {e}")
+            self.close_driver(self.exchange)
+            return 0.0, pd.DataFrame()
 
     def parse_table(self, elem):
-        data = elem.find("tbody").find_all("tr")
-        rows = []
+        try:
+            data = elem.find("tbody").find_all("tr")
+            rows = []
 
-        for row in data:
-            cols = row.find_all("td")
-            dt = {
-                "symbol": cols[0].text.strip(),
-                "qty": cols[1].text,
-                "price": cols[2].text,
-                "cap": float(cols[5].text.replace(",", "")),
-                "unit_cost": float(cols[6].text.replace(",", "")),
-                "total_cost": float(cols[7].text.replace(",", "")),
-                "pnl": float(cols[8].text.replace(",", "").strip()[1:])
-                * (1 if (cols[8].text)[0] == "+" else -1),
-                "pnl_%": float(cols[9].text.replace(",", "").strip()[1:])
-                * (1 if (cols[9].text)[0] == "+" else -1),
-            }
-            rows.append(dt)
-        df = pd.DataFrame(rows)
-        return df
+            try:
+                for row in data:
+                    cols = row.find_all("td")
+                    dt = {
+                        "symbol": cols[0].text.strip(),
+                        "qty": cols[1].text,
+                        "price": cols[2].text,
+                        "cap": float(cols[5].text.replace(",", "")),
+                        "unit_cost": float(cols[6].text.replace(",", "")),
+                        "total_cost": float(cols[7].text.replace(",", "")),
+                        "pnl": float(cols[8].text.replace(",", "").strip()[1:])
+                        * (1 if (cols[8].text)[0] == "+" else -1),
+                        "pnl_%": float(
+                            cols[9].text.replace(",", "").strip()[1:]
+                        )  # noqa: E501
+                        * (1 if (cols[9].text)[0] == "+" else -1),
+                    }
+                    rows.append(dt)
+            except ValueError as e:
+                logging.error(f"Error parsing row: {e}")
+
+            df = pd.DataFrame(rows)
+            return df
+        except Exception as e:
+            logging.error(f"Error parsing table: {e}")
+            return pd.DataFrame()
 
 
 if __name__ == "__main__":
+    logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
     parser = argparse.ArgumentParser("FIRSTRADE INFO")
     args = parser.parse_args()
-    client = Firstrade("FIRSTRADE")
-    client.login()
-    cash, stock = client.info()
-    print(f"cash: {round(cash, 3)}")
-    print(f"cap: {round(stock.cap.sum(), 3)}")
-    print(f"asset: {round(cash + stock.cap.sum(), 3)}")
-    print(f"pnl: {round(stock.pnl.sum(), 3)}")
-    print(f"cost: {round(stock.total_cost.sum(), 3)}")
-    print(f"pnl_%: {round(stock.pnl.sum()/stock.total_cost.sum() * 100, 3)}")
-    client.close_driver()
+    exchange = "FIRSTRADE"
+    client = Firstrade(exchange)
+    try:
+        client.login()
+        cash, stock = client.info()
+        print(f"cash: {round(cash, 3)}")
+        print(f"cap: {round(stock.cap.sum(), 3)}")
+        print(f"asset: {round(cash + stock.cap.sum(), 3)}")
+        print(f"pnl: {round(stock.pnl.sum(), 3)}")
+        print(f"cost: {round(stock.total_cost.sum(), 3)}")
+        print(
+            f"pnl_%: {round(stock.pnl.sum()/stock.total_cost.sum() * 100, 3)}"
+        )  # noqa: E501
+    except Exception as e:
+        logging.error(f"Error occurred: {e}")
+    finally:
+        client.close_driver(exchange)
